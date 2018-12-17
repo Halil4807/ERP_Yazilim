6b4d220f0f4ae8fb9eb510c5b269f2ee9cc34a23
E-Ticaret <fethiyeli1996@gmail.com>
2018-12-17 22:15:27 +0300

    Admin Login (Session)

--- nedmin/netting/islem.php
+++ nedmin/netting/islem.php
@@ -1,5 +1,38 @@
 <?php 
+ob_start();
+session_start();
 include 'baglan.php';
+
+if (isset($_POST['admingiris'])) {
+
+	$kullanici_mail=$_POST['kullanici_mail'];
+	$kullanici_password=md5($_POST['kullanici_password']);
+
+	$kullanicisor=$db->prepare("SELECT * FROM kullanici where kullanici_mail=:mail and kullanici_password=:password and kullanici_yetki=:yetki");
+	$kullanicisor->execute(array(
+		'mail' => $kullanici_mail,
+		'password' => $kullanici_password,
+		'yetki' => 5
+		));
+
+	echo $say=$kullanicisor->rowCount();
+
+	if ($say==1) {
+				
+		$_SESSION['kullanici_mail']=$kullanici_mail;
+		header("Location:../production/index.php");
+		exit;
+
+
+
+	} else {
+
+		header("Location:../production/login.php?durum=no");
+		exit;
+	}
+	
+
+}
 
 
 if (isset($_POST['genelayarkaydet'])) {

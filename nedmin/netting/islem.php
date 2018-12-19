770cc171c6c8ff6553fe0e873a2d52df03e1ceff
E-Ticaret <fethiyeli1996@gmail.com>
2018-12-20 01:27:16 +0300

    Müşteri Kayıt Ol

--- nedmin/netting/islem.php
+++ nedmin/netting/islem.php
@@ -3,6 +3,79 @@ ob_start();
 session_start();
 include 'baglan.php';
 
+
+
+if(isset($_POST['kullanicikaydet'])){
+
+	// htmlspecialchars fonksiyonu script gibi 
+	//şifre kırma yöntemlerine karşı koruma sağlıyor
+	$kullanici_adsoyad=htmlspecialchars($_POST['kullanici_adsoyad']);
+	$kullanici_mail=htmlspecialchars($_POST['kullanici_mail']);
+
+	$kullanici_passwordone=htmlspecialchars($_POST['kullanici_passwordone']);
+	$kullanici_passwordtwo=htmlspecialchars($_POST['kullanici_passwordtwo']);
+
+	$kullanicisor=$db->prepare("select * from kullanici where kullanici_mail=:mail");
+	$kullanicisor->execute(array(
+		'mail' => $kullanici_mail
+		));
+	$kayittsor=$db->prepare("select * from kullanici");
+	$kayittsor->execute(array());
+	$kayittsay=$kayittsor->rowCount();
+	//dönen satır sayısını belirtir
+	$say=$kullanicisor->rowCount();
+	if ($say==0) 
+	{
+		if($kullanici_passwordone==$kullanici_passwordtwo)
+		{
+			if($kullanici_passwordone>=6)
+			{
+					//md5 fonksiyonu şifreyi md5 şifreli hale getirir.
+				$password=md5($kullanici_passwordone);
+
+				$kullanici_yetki=1;
+
+					//Kullanıcı kayıt işlemi yapılıyor...
+				$kullanicikaydet=$db->prepare("INSERT INTO kullanici SET
+					kullanici_id=:kullanici_id,
+					kullanici_adsoyad=:kullanici_adsoyad,
+					kullanici_mail=:kullanici_mail,
+					kullanici_password=:kullanici_password,
+					kullanici_yetki=:kullanici_yetki
+					");
+				$insert=$kullanicikaydet->execute(array(
+					'kullanici_id'=>$kayittsay,
+					'kullanici_adsoyad' => $kullanici_adsoyad,
+					'kullanici_mail' => $kullanici_mail,
+					'kullanici_password' => $password,
+					'kullanici_yetki' => $kullanici_yetki
+					));
+				if ($insert) 
+				{
+					header("Location:../../index.php?durum=loginbasarili");
+				} 
+				else 
+				{
+					header("Location:../../register.php?durum=basarisiz");
+				}
+			}
+			else
+			{
+				header("Location:../../register.php?durum=eksiksifre");
+			}
+		}
+		else
+		{
+			header("Location:../../register.php?durum=farklisifre");
+		}
+		
+	}
+	else 
+	{
+		header("Location:../../register.php?durum=mukerrerkayit");
+	}
+}
+
 if (isset($_POST['admingiris'])) {
 
 	$kullanici_mail=$_POST['kullanici_mail'];
@@ -18,7 +91,7 @@ if (isset($_POST['admingiris'])) {
 	echo $say=$kullanicisor->rowCount();
 
 	if ($say==1) {
-				
+
 		$_SESSION['kullanici_mail']=$kullanici_mail;
 		header("Location:../production/index.php");
 		exit;
@@ -176,4 +249,7 @@ if($_GET['kullanicisil']==ok){
 		header("Location:../production/kullanici.php?sil=no");
 	}
 }
+
+
+
 ?>
